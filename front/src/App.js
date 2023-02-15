745b65c535525a84c73fd2998b13fcd145549ab9
arielmadar1 <arielmadar148@gmail.com>
2023-02-15 17:11:24 +0200

    front&back together

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createContext, useContext, useEffect, useState } from "react";
 import './App.css';
 import { Cloudinary } from "@cloudinary/url-gen";
@@ -25,29 +24,6 @@ function App() {
       </div>
     </Storage.Provider>
 
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> b49f4e4 (backend basic functions)
   );
 }
 

bd53c357cbb23b4a3ed45457c506c4d9415e37bd
anyap2 <pashover.anya2014@gmail.com>
2023-03-15 20:01:01 +0200

    some design

--- front/src/pages/Enter.jsx
+++ front/src/pages/Enter.jsx
@@ -11,34 +11,39 @@ const Enter = () => {
     const cloudinaryRef = useRef()
     const widgetRef = useRef()
     return (
-        <div className="relative isolate  flex lg:flex-row bg--50 w-full h-auto py-16 sm:py-24 flex-col " >
-            <Lottie className="w-96 h-auto ml-14" animationData={Emotion} loop={true} />
-            <div className='ml-6 max-w-xs'>
-                <h3 className='text-4xl'>
-                Be a Star
+        <div className="w-full h-screen flex flex-row justify-evenly items-center sm:py-24 flex-col " >
+            <Lottie className="w-1/3 h-auto " animationData={Emotion} loop={true} />
+
+            <div className='h-full w-1/4  flex flex-col justify-center'>
+                <h3 className='text-4xl pb-4'>
+                    Be a Star
                 </h3>
-                <p className='mt-6 mb-6'>
-                Our stars help us teach children with autism about emotions, by sending images, video and voices depicting diverse ways of emotion expression                </p>
-                <div className='w-auto flex justify-end '>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+
+                <p className='text-2xl'>
+                    Our stars help us teach children with autism about emotions, by sending images, video and voices depicting diverse ways of emotion expression
+                </p>
+                <div className='w-auto flex justify-center mt-3'>
+                    <button className="bg-blue-500 p-3 hover:bg-blue-700 text-white font-bold px-5 rounded-full">
                         <NavLink to={"/emotionlist"} style={{ textDecoration: 'none' }} onClick={() => widgetRef.current.open()}>
-                        Star
+                            Star
                         </NavLink>
                     </button>
                 </div>
             </div>
-            <div className='ml-6 max-w-xs'>
-                <h3 className='text-4xl'>
-                Be an Evaluator
 
+            <div className=' h-full w-1/4 flex flex-col justify-center'>
+                <h3 className='text-4xl pb-4'>
+                    Be an Evaluator
                 </h3>
-                <p className='mt-6 mb-6'>
-                Evaluators help us teach children with autism about emotions, by evaluating and validating the media we collect, making sure it is appropriate and clearly expressed before using it in our program
+
+                <p className='text-2xl'>
+                    Evaluators help us teach children with autism about emotions, by evaluating and validating the media we collect, making sure it is appropriate and clearly expressed before using it in our program
                 </p>
-                <div className='w-auto flex justify-end  '>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex justify-end">
+
+                <div className=' flex justify-center mt-3'>
+                    <button className="bg-blue-500 p-3 hover:bg-blue-700 text-white font-bold px-5 rounded-full flex justify-end">
                         <NavLink to={"/checker/0"} className='caret-lime-900'>
-                        Evaluator
+                            Evaluator
                         </NavLink>
                     </button>
                 </div>
